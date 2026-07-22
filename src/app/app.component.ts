7b35b63a7ddb8b13479241ae698e0de1a26c71b9
Adel Bikkenin <autopluskazan@gmail.com>
2026-07-22 22:01:55 +0300

    Completed homework 18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import './training';
 import { Color } from '../enums/color'
 import { Collection } from './collection';
+import { iService } from '../interfaces/iService';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 
-export class AppComponent {
-  constructor() {
+export class AppComponent implements OnInit, OnDestroy {
+  public isLoading: boolean = true;
+  public currentDate: string = new Date().toLocaleString('ru-RU');
+
+  public readonly companyName: string = 'румтибет';
+
+  public selectedLocation: string = '';
+  public selectedDate: string = '';
+  public selectedParticipants: string = '';
+  public liveText: string = '';
+
+  private loadingTimerId: number | undefined;
+  private dateTimerId: number | undefined;
+
+  public clickCount: number = 0;
+
+  public isDateVisible: boolean = true;
+
+  public readonly services: iService[] = [
+    {
+      id:1, 
+      title: 'Опытный гид', 
+      description: 'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.',
+      icon: '/images/guide-icon.svg',
+      alt: 'Иконка опытного гида'
+    },
+    {
+      id:2, 
+      title: 'Безопасный поход', 
+      description: 'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.',
+      icon: '/images/shield-icon.svg',
+      alt: 'Иконка безопасного похода'
+    },
+    {
+      id:3, 
+      title: 'Лояльные цены', 
+      description: 'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.',
+      icon: '/images/price-icon.svg',
+      alt: 'Иконка бирки'
+    }
+  ];
+
+  public constructor() {
     this.saveLastVisit();
     this.saveVisitsCount();
 
@@ -29,9 +72,47 @@ export class AppComponent {
     console.log(phoneNumbers.getAll());
   }
 
-  companyName: string = 'румтибет';
+  public ngOnInit(): void {
+    this.loadingTimerId = window.setTimeout(() => {
+      this.isLoading = false;
+    }, 2000);
 
-  isPrimaryColor(color: Color): boolean {
+    this.dateTimerId = window.setInterval(() => {
+      this.currentDate = new Date().toLocaleString('ru-RU');
+  }, 1000);
+  }
+
+  public ngOnDestroy(): void {
+    if (this.loadingTimerId !== undefined) {
+      window.clearTimeout(this.loadingTimerId);
+    }
+
+    if (this.dateTimerId !== undefined) {
+      window.clearInterval(this.dateTimerId);
+    }
+  }
+
+  public incrementCount(): void {
+    this.clickCount += 1;
+  }
+
+  public decrementCount(): void {
+    if (this.clickCount > 0) {
+      this.clickCount -= 1;
+    }
+  }
+
+  public toggleHeaderWidget(): void {
+    this.isDateVisible = !this.isDateVisible;
+  }
+
+  public chooseTour(): void {
+    console.log('Локация:', this.selectedLocation);
+    console.log('Дата:', this.selectedDate);
+    console.log('Участники:', this.selectedParticipants);
+}
+
+  private isPrimaryColor(color: Color): boolean {
     return (
       color === Color.RED ||
       color === Color.GREEN ||
@@ -39,11 +120,11 @@ export class AppComponent {
     );
   }
 
-  saveLastVisit(): void {
+  private saveLastVisit(): void {
     localStorage.setItem('lastVisit', new Date().toISOString());
   }
 
-  saveVisitsCount(): void {
+  private saveVisitsCount(): void {
     const visits = Number(localStorage.getItem('visits')) || 0;
 
     localStorage.setItem('visits', String(visits + 1));
